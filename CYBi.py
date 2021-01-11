90d1eb40d0b54486e3751a04f7690ab44a04d546
rmarkscott <mscott@fhu.edu>
2021-01-10 20:02:18 -0600

    a

--- CYBi.py
+++ CYBi.py
@@ -64,6 +64,8 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online,
                               activity=discord.Activity(type=discord.ActivityType.watching, name="over FHU CYB"))
 
+    
+
 '''
 ------------------------------------------------------------------------
 
